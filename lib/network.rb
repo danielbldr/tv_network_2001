dc6d162db8e0185096588a65da38a1a329ef0d8f
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-11 10:13:10 -0700

    Complete iteration 3

--- lib/network.rb
+++ lib/network.rb
@@ -1,1 +1,30 @@
+class Network
+  attr_reader :name, :shows
 
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    found_characters = []
+    @shows.find_all do |show|
+      show.characters.find_all do |character|
+        found_characters << character if character.salary > 500_000 && character.name == character.name.upcase
+      end
+    end
+    found_characters
+  end
+
+  def actors_by_show
+    shows = {}
+    @shows.map do |show|
+      shows[show] = show.characters.map {|character| character.actor}
+    end
+    shows
+  end
+end
